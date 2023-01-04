ce2d49972bf24a1b8ed22ed1fa90d69a3df4ec2e
blueforest529 <imcheong001@gmail.com>
2023-01-04 22:03:43 +0900

    테스트 페이지 완성

--- client/src/components/Test.vue
+++ client/src/components/Test.vue
@@ -1,75 +1,120 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div>
-        <alert :message="message" v-if="showMessage"></alert>
-        
-        테스트 
-        
+    <div style="width:30%; margin: auto;">
+        <b-button-group style="margin-bottom:20px;">
+            <b-button :pressed="true" variant="success" value="0" v-on:click="changeType()" style="margin-right: 15px;"> 단어 </b-button> 
+            <b-button :pressed="false" variant="success" value="1" v-on:click="changeType()"> 뜻 </b-button>
+        </b-button-group>
+      
+        <div v-show="questionIndex < length">
+            <b-card style="max-width: 100%;" class="mb-2">
+                <div v-for="(item, index) in allQuestion">
+                    <div v-show="index === questionIndex">
+                        <h2>{{ item }}</h2>
+                        <input type="text" v-on:input="updateAnswer" placeholder="답을 입력하세요">
+                    </div>
+                </div>
 
+                <b-button-group style="margin-top: 25px;">
+                    <b-button  class="btn btn-warning btn-sm" v-if="questionIndex > 0" variant="primary" v-on:click="prev">
+                        이전
+                    </b-button>
+                    <b-button  class="btn btn-warning btn-sm" v-on:click="next" variant="primary">
+                        다음
+                    </b-button>
+                </b-button-group>
+            </b-card>
+        </div>
 
-      </div>
+        <div>
+            <div v-show="questionIndex === length">
+                <h2>
+                    테스트가 끝났습니다!
+                </h2>
+                <p>
+                    당신의 점수는 : {{ score() }} / {{length }}
+                </p>
+                <b-button :pressed="false" variant="success" v-on:click="reset()"> 초기화 </b-button>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import axios from 'axios';
-import Alert from './Alert.vue';
 
 export default {
   data() {
     return {
-      word: [],
-      idiom: [],
-      perPage: 15,
-      currentPage: 1,
-      fields: [
-          { key: 'word', label: '단어'},
-          { key: 'mean', label: '뜻'} 
-        ],
-      addBookForm: {
-        title: '',
-        author: '',
-        read: [],
-      },
-      editForm: {
-        id: '',
-        title: '',
-        author: '',
-        read: [],
-      },
-      message: '',
-      showMessage: false,
+        quiz: [],
+        answer : [],
+        word: [],
+        idiom: [],
+        allQuestion : [],
+        allAnswers : [],
+        type : 0,
+        length: 0,
+        questionIndex: 0,
+        userResponses: Array(this.length).fill(false)
     };
-  },
-  components: {
-    alert: Alert,
-  },
-  computed:{
-    rows() {
-        return this.volt.length
-    },
-    editableFields() {
-        return this.fields.filter(field => { return field.editable === true })
-    }
   },
   created() {
     this.getExam();
   },
   methods: {
-    editItem(item) { console.log(item); },
+    changeType(e) {
+        this.type = parseInt(e.target.value);
+    },
+    updateAnswer(event) {
+        this.userResponses[this.questionIndex] = event.target.value;
+    },
+    next() {
+      this.questionIndex++;
+    },
+    prev() {
+      this.questionIndex--;
+    },
+    reset(){
+        this.allAnswers = [];
+        this.allQuestion = [];
+        this.questionIndex = 0;
+        this.getExam();
+    },
+    score() {
+        var score = 0;
+        if (this.questionIndex >= this.length) {
+            for (var i in this.allAnswers) {
+                if (this.allAnswers[i] == this.userResponses[i]) {
+                    score++;
+                }
+            }
+            return score;
+        }
+        return false;
+    },  
     getExam() {
         const path = 'http://127.0.0.1:5000/exam';
         axios.get(path)
-            .then((res) => {
-                this.word = res.data['word'];
-                this.idiom = res.data['idiom'];
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        .then((res) => {
+            this.word = Object.values(res.data['word']);
+            this.idiom = Object.values(res.data['idiom']);
+        }).then(()=>{
+            this.length = this.word.length + this.idiom.length;
+            var queType = this.type ? 'word' : 'mean';
+            var ansType = this.type ? 'mean' : 'word';
+
+            for (var i in this.word) {
+                this.allQuestion.push(this.word[i][queType]);
+                this.allAnswers.push(this.word[i][ansType]);
+            }
+
+            for (var i in this.idiom) {
+                this.allQuestion.push(this.idiom[i][queType]);
+                this.allAnswers.push(this.idiom[i][ansType]);
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+        } 
     }
-  }
 };
 </script>
--- server/exam.py
+++ server/exam.py
@@ -1,4 +1,3 @@
-import json
 from flask import request, jsonify
 from flask_restx import Resource, Namespace
 import databases
@@ -24,56 +23,9 @@ class ExamPost(Resource):
         result['word'] = self.cusur.fetchall()
 
         #숙어
-        sql = 'select idiom, mean from idiom order by rand() limit 0,5;'
+        sql = 'select idiom as word, mean from idiom order by rand() limit 0,5;'
         self.cusur.execute(sql)
         result['idiom'] = self.cusur.fetchall()
 
         return jsonify(result)
 
-
-# @Exam.route('/<int:id>')
-# @Exam.doc(params={'id': 'seq'})
-# @Exam.doc(responses={202: 'Success'})
-# @Exam.doc(responses={500: 'Failed'})
-# class ExamAPI(Resource):
-#     def __init__(self, cusur) :
-#         self.cusur = databases.db_conn()
-#         self.db = databases.db
-
-
-#     def get(self, id):
-#         """특정 토익 숙어를 불러옵니다"""
-#         sql = 'select * from idiom where seq='+str(id)+';'
-#         self.cusur.execute(sql)
-#         result = self.cusur.fetchall()
-#         return jsonify(result)
-        
-
-#     def put(self, id):
-#         """특정 토익 숙어를 업데이트 합니다"""
-#         data = request.form.to_dict()
-#         data = next(iter(data))
-#         data = json.loads(data)
-
-#         idiom = data.get('idiom')
-#         mean = data.get('mean')
-
-#         sql = "update idiom set idiom='%s', mean='%s'"%(idiom, mean) + "where seq="+str(id)+";"
-
-#         self.cusur.execute(sql)
-#         self.db.commit()
-
-#         return {
-#             "result" : "success"
-#         }
-
-    
-#     def delete(self, id):
-#         """특정 토익 숙어를 삭제 합니다"""
-#         sql = "delete from idiom where seq="+str(id)+";"
-#         self.cusur.execute(sql)
-#         self.db.commit()
-        
-#         return {
-#             "result" : "success"
-#         }
